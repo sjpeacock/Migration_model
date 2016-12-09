a3d149d3b5d2c37976ee0323c812e687e6c24023
Stephanie J Peacock <stephanie.j.peacock@gmail.com>
2016-12-09 15:47:36 -0700

    Add files via upload

---
+++ js/MigVsStat_longHTML.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#MigVsStat_longHTML').scianimator({
+	    'images': ['images/MigVsStat_longHTML1.png', 'images/MigVsStat_longHTML2.png', 'images/MigVsStat_longHTML3.png', 'images/MigVsStat_longHTML4.png', 'images/MigVsStat_longHTML5.png', 'images/MigVsStat_longHTML6.png', 'images/MigVsStat_longHTML7.png', 'images/MigVsStat_longHTML8.png', 'images/MigVsStat_longHTML9.png', 'images/MigVsStat_longHTML10.png', 'images/MigVsStat_longHTML11.png', 'images/MigVsStat_longHTML12.png', 'images/MigVsStat_longHTML13.png', 'images/MigVsStat_longHTML14.png', 'images/MigVsStat_longHTML15.png', 'images/MigVsStat_longHTML16.png', 'images/MigVsStat_longHTML17.png', 'images/MigVsStat_longHTML18.png', 'images/MigVsStat_longHTML19.png', 'images/MigVsStat_longHTML20.png', 'images/MigVsStat_longHTML21.png', 'images/MigVsStat_longHTML22.png', 'images/MigVsStat_longHTML23.png', 'images/MigVsStat_longHTML24.png', 'images/MigVsStat_longHTML25.png', 'images/MigVsStat_longHTML26.png', 'images/MigVsStat_longHTML27.png', 'images/MigVsStat_longHTML28.png', 'images/MigVsStat_longHTML29.png', 'images/MigVsStat_longHTML30.png', 'images/MigVsStat_longHTML31.png', 'images/MigVsStat_longHTML32.png', 'images/MigVsStat_longHTML33.png', 'images/MigVsStat_longHTML34.png', 'images/MigVsStat_longHTML35.png', 'images/MigVsStat_longHTML36.png', 'images/MigVsStat_longHTML37.png', 'images/MigVsStat_longHTML38.png', 'images/MigVsStat_longHTML39.png', 'images/MigVsStat_longHTML40.png', 'images/MigVsStat_longHTML41.png', 'images/MigVsStat_longHTML42.png', 'images/MigVsStat_longHTML43.png', 'images/MigVsStat_longHTML44.png', 'images/MigVsStat_longHTML45.png', 'images/MigVsStat_longHTML46.png', 'images/MigVsStat_longHTML47.png', 'images/MigVsStat_longHTML48.png', 'images/MigVsStat_longHTML49.png', 'images/MigVsStat_longHTML50.png', 'images/MigVsStat_longHTML51.png', 'images/MigVsStat_longHTML52.png', 'images/MigVsStat_longHTML53.png', 'images/MigVsStat_longHTML54.png', 'images/MigVsStat_longHTML55.png', 'images/MigVsStat_longHTML56.png', 'images/MigVsStat_longHTML57.png', 'images/MigVsStat_longHTML58.png', 'images/MigVsStat_longHTML59.png', 'images/MigVsStat_longHTML60.png'],
+	    'width': 800,
+	    'delay': 100,
+	    'loopMode': 'loop'
+	});
+	$('#MigVsStat_longHTML').scianimator('play');
+    });
+})(jQuery);
